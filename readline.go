65d6f1d0cebee80c26ff655786aca236d70c6299
Sebastien Binet <binet@farnsworth>
2010-09-02 16:53:43 +0200

    binding more of the readline API

--- readline.go
+++ readline.go
@@ -2,12 +2,16 @@
 
 package readline
 
+// TODO:
+//  implement a go-oriented command completion
+
 // #include <stdio.h>
 // #include <stdlib.h>
 // #include <readline/readline.h>
 // #include <readline/history.h>
 import "C"
 import "unsafe"
+import "os"
 
 func ReadLine(prompt *string) *string {
 	var p *C.char;
@@ -28,6 +32,50 @@ func ReadLine(prompt *string) *string {
 
 func AddHistory(s string) {
 	p := C.CString(s);
-	C.add_history(p);
-	C.free(unsafe.Pointer(p))
+	defer C.free(unsafe.Pointer(p))
+	C.add_history(p);	
 }
+
+// Parse and execute single line of a readline init file.
+func ParseAndBind(s string) {
+	p := C.CString(s)
+	defer C.free(unsafe.Pointer(p))
+	C.rl_parse_and_bind(p)
+}
+
+// Parse a readline initialization file.
+// The default filename is the last filename used.
+func ReadInitFile(s string) os.Errno {
+	p := C.CString(s)
+	defer C.free(unsafe.Pointer(p))
+	errno := C.rl_read_init_file(p)
+	return os.Errno(errno)
+}
+
+// Load a readline history file.
+// The default filename is ~/.history.
+func ReadHistoryFile(s string) os.Errno {
+	p := C.CString(s)
+	defer C.free(unsafe.Pointer(p))
+	errno := C.read_history(p)
+	return os.Errno(errno)
+}
+
+var (
+	HistoryLength = -1
+)
+
+// Save a readline history file.
+// The default filename is ~/.history.
+func WriteHistoryFile(s string) os.Errno {
+	p := C.CString(s)
+	defer C.free(unsafe.Pointer(p))
+	errno := C.write_history(p)
+	if errno==0 && HistoryLength >= 0 {
+		errno = C.history_truncate_file(p, C.int(HistoryLength))
+	}
+	return os.Errno(errno)
+}
+
+
+/* EOF */
